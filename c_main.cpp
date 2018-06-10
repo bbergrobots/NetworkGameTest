3fb211d38636a24c710896b25960dc7999a535f5
bbergrobots <brendan_berg@aol.com>
2018-06-10 11:55:09 +0200

    Initial commit

---
+++ c_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "NetworkGameTest Client Boot" << std::endl;
+    std::cout << "===========================" << std::endl;
+    std::cout << std::endl << std::endl;
+
+    return 0;
+}
---
+++ s_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "NetworkGameTest Server Boot" << std::endl;
+    std::cout << "===========================" << std::endl;
+    std::cout << std::endl << std::endl;
+
+    return 0;
+}
